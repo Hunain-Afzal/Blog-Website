793b2968c8cf6a4eaedf991bd6afb2a4e3738c04
Hunain Afzal <hunainafzal499@gmail.com>
2026-09-03 22:48:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+/* =========================================
+   Welcome Alert
+========================================= */
+let currentHour = new Date().getHours();
+
+if (currentHour < 12) {
+    alert("Good Morning! Welcome to My Lifestyle Blog");
+}
+else if (currentHour < 18) {
+    alert("Good Afternoon! Welcome to My Lifestyle Blog");
+}
+else {
+    alert("Good Evening! Welcome to My Lifestyle Blog");
+}
+/* =========================================
+   SUBSCRIBE FORM VALIDATION
+========================================= */
+
+/* Form aur input elements ko select karna */
+let subscribeForm = document.getElementById("subscribe-form");
+let emailInput = document.getElementById("email");
+let messageInput = document.getElementById("message");
+
+subscribeForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let email = emailInput.value;
+    let message = messageInput.value;
+
+    if (email === "" || message === "") {
+
+        alert("Please fill in all fields.");
+
+    }
+    else {
+
+        alert("Thank you for subscribing!");
+
+        subscribeForm.reset();
+    }
+
+});
+/* =========================================
+   BACK TO TOP BUTTON
+========================================= */
+
+let backToTopButton = document.getElementById("back-to-top");
+
+
+window.addEventListener("scroll", function() {
+
+    if (window.scrollY > 300) {
+
+        backToTopButton.style.display = "block";
+
+    } else {
+
+        backToTopButton.style.display = "none";
+    }
+
+});
+
+backToTopButton.addEventListener("click", function() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
